0b85df77ce38decf7e68bd958b885c46ebea5f9f
modestas <modestukasai@gmail.com>
2017-07-19 22:49:51 +0300

    Validation fixes for taxes resource

--- service/src/main/tax_manager/api/TaxesResource.go
+++ service/src/main/tax_manager/api/TaxesResource.go
@@ -131,12 +131,38 @@ func SaveNewMunicipalityTax(factory factory.ApplicationFactory) (httprouter.Hand
 
 		fromTime, fromTimeParseError := time.Parse(tax_manager.DEFAULT_DATE_FORMAT, saveTaxRequest.From)
 		if fromTimeParseError != nil {
-			fmt.Fprint(w, Marshal(ErrorResponse{ErrorMessage: fmt.Sprintf("Property `from` has to be in format `%s`", tax_manager.DEFAULT_DATE_FORMAT)}))
+			fmt.Fprint(w, Marshal(ErrorResponse{ErrorMessage: fmt.Sprintf("Property `from` has to be in format `%s`",
+				tax_manager.DEFAULT_DATE_FORMAT)}))
+			return
+		}
+		if fromTime.IsZero() {
+			fmt.Fprint(w, Marshal(
+				ErrorResponse{ErrorMessage: fmt.Sprintf("Property `from` has to be set")}))
+			return
 		}
 
 		toTime, toTimeParseError := time.Parse(tax_manager.DEFAULT_DATE_FORMAT, saveTaxRequest.To)
 		if toTimeParseError != nil {
-			fmt.Fprint(w, Marshal(ErrorResponse{ErrorMessage: fmt.Sprintf("Property `to` has to be in format `%s`", tax_manager.DEFAULT_DATE_FORMAT)}))
+			fmt.Fprint(w, Marshal(ErrorResponse{ErrorMessage: fmt.Sprintf("Property `to` has to be in format `%s`",
+				tax_manager.DEFAULT_DATE_FORMAT)}))
+			return
+		}
+		if toTime.IsZero() {
+			fmt.Fprint(w, Marshal(
+				ErrorResponse{ErrorMessage: fmt.Sprintf("Property `to` has to be set")}))
+			return
+		}
+
+		if saveTaxRequest.Value == 0 {
+			fmt.Fprint(w, Marshal(
+				ErrorResponse{ErrorMessage: fmt.Sprintf("Property `value` has to be set")}))
+			return
+		}
+
+		if len(saveTaxRequest.TaxType) == 0 {
+			fmt.Fprint(w, Marshal(
+				ErrorResponse{ErrorMessage: fmt.Sprintf("Property `taxType` has to be set")}))
+			return
 		}
 
 		newTax := tax.Tax{
